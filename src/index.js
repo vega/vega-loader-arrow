3bb32ba54132091b7d1fc93b1a2777d6c36c510a
Jeffrey Heer <jeffrey.heer@gmail.com>
2024-09-16 08:09:14 -0700

    feat: use flechette for Arrow decoding. (#365)
    * feat: Use flechette for Arrow decoding.
    
    * chore: Bump flechette version.
    
    * chore: Bump flechette dependency to 1.0.0.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { tableFromIPC } from '@uwdata/flechette';
+
+/**
+ * Load a data set in Apache Arrow format for use in Vega.
+ * @param {*} data Either an already parsed table (created by the
+ *  `@uwdata/flechette` or `apache-arrow` libraries) or Arrow IPC
+ *  binary data as an `ArrayBuffer`, `Uint8Array`, or `Uint8Array[]`.
+ * @returns {Record<string,any>[]} An array of data objects representing
+ *  rows of a data table.
+ */
+export default function arrow(data) {
+  return (isArrowTable(data) ? data : decodeIPC(data)).toArray();
+}
+
+function isArrowTable(data) {
+  return data && data.schema && Array.isArray(data.schema.fields)
+    && typeof data.toArray === 'function';
+}
+
+function decodeIPC(data, options = { useProxy: true }) {
+  return tableFromIPC(data, options);
+}
+
+arrow.responseType = 'arrayBuffer';
--- test/arrow-test.js
+++ test/arrow-test.js
@@ -1,35 +1,16 @@
-const tape = require('tape');
-const {Table, tableFromIPC, tableFromArrays, tableToIPC, vectorFromArray, Schema, Field, Float32} = require('apache-arrow');
-const arrow = require('../');
+import { default as tape } from 'tape';
+import { tableFromIPC as arrowJSTableFromIPC } from 'apache-arrow';
+import {
+  columnFromArray, float32, tableFromArrays, tableFromColumns,
+  tableFromIPC, tableToIPC
+} from '@uwdata/flechette';
+import arrow from '../src/index.js';
 
-function testProxyMatch(test, table, data) {
-  const datum = data[0];
-  const nrows = table.numRows;
-  const fields = [];
-
-  test.equal(data.length, nrows);
-
-  // collect fields from first data object
-  for (let f in datum) fields.push(f);
-
-  // test that fields match table schema
-  test.deepEqual(
-    fields,
-    table.schema.fields.map(_ => _.name)
-  );
-
-  // test that object properties match table data
-  for (let row=0; row<nrows; ++row) {
-    const d = data[row];
-    fields.forEach(f => test.deepEqual(d[f], table.getChild(f).get(row)));
-  }
-}
-
-tape('Arrow reader should read Apache Arrow data', function(test) {
+tape('Arrow reader should read Arrow IPC data', test => {
   const nrows = 20;
-  const blob = generateArrowData(nrows);
-  const table = tableFromIPC(blob); // Arrow API Table
-  const data = arrow(blob); // Vega-friendly objects for each row
+  const bytes = generateIPCData(nrows);
+  const table = tableFromIPC(bytes); // Flechette Table
+  const data = arrow(bytes); // Vega-friendly objects for each row
 
   // test that table and proxy objects match
   testProxyMatch(test, table, data);
@@ -45,16 +26,16 @@ tape('Arrow reader should read Apache Arrow data', function(test) {
 
   // writes to derived columns should work as usual
   test.doesNotThrow(() => { datum[g] = 'foo'; });
-  test.ok(datum.hasOwnProperty(g));
+  test.ok(Object.hasOwn(datum, g));
   test.equal(datum[g], 'foo');
 
   test.end();
 });
 
-tape('Arrow reader should accept pre-parsed Table', function(test) {
+tape('Arrow reader should accept pre-parsed Flechette Table', test => {
   const nrows = 20;
-  const blob = generateArrowData(nrows);
-  const table = tableFromIPC(blob); // Arrow API Table
+  const bytes = generateIPCData(nrows);
+  const table = tableFromIPC(bytes); // Flechette Table
   const data = arrow(table); // Vega-friendly objects for each row
 
   // test that table and proxy objects match
@@ -63,67 +44,94 @@ tape('Arrow reader should accept pre-parsed Table', function(test) {
   test.end();
 });
 
-tape('Arrow reader should handle non-unique column names', function(test) {
-  const nrows = 10;
-  const blob = generateDuplicateArrowData(nrows);
-  const table = tableFromIPC(blob); // Arrow API Table
+tape('Arrow reader should accept pre-parsed Apache Arrow Table', test => {
+  const nrows = 20;
+  const bytes = generateIPCData(nrows);
+  const table = arrowJSTableFromIPC(bytes); // Arrow JS Table
+  const data = arrow(table); // Vega-friendly objects for each row
 
-  const data = arrow(blob); // Vega-friendly objects for each row
-  const datum = data[0];
+  // test that table and proxy objects match
+  testProxyMatch(test, table, data);
+
+  test.end();
+});
+
+tape('Arrow reader should handle non-unique column names', test => {
+  const nrows = 10;
+  const bytes = generateDuplicateIPCData(nrows);
+  const table = tableFromIPC(bytes); // Arrow API Table
+
+  const data = arrow(bytes); // Vega-friendly objects for each row
 
   test.equal(data.length, table.numRows);
 
   // collect fields from first data object
   const fields = [];
-  for (let f in datum) fields.push(f);
+  for (const f in data[0]) fields.push(f);
 
   // test that duplicated name is suppressed in row proxy
   test.equal(fields.length, 1);
   test.equal(table.schema.fields.map(_ => _.name).length, 2);
 
   // test that row proxy field names privilege lower index column
-  test.equal(datum.precipitation, table.getChildAt(0).get(0));
+  test.deepEquals(
+    Array.from(data, v => v[fields[0]]),
+    Array.from(table.getChildAt(0))
+  );
 
   test.end();
 });
 
-function generateArrowData(nrows) {
-  const precipitationData = Array.from(
+function testProxyMatch(test, table, data) {
+  const nrows = table.numRows;
+  const fields = [];
+
+  test.equal(data.length, nrows);
+
+  // collect fields from first data object
+  for (const f in data[0]) fields.push(f);
+
+  // test that fields match table schema
+  test.deepEqual(
+    fields,
+    table.schema.fields.map(_ => _.name)
+  );
+
+  // test that object properties match table data
+  for (const f of fields) {
+    test.deepEqual(
+      Array.from(data, v => v[f]),
+      Array.from(table.getChild(f))
+    );
+  }
+}
+
+function generateIPCData(nrows) {
+  const precipitation = Array.from(
     {length: nrows},
     () => Number((Math.random() * 20).toFixed(1))
   );
 
-  const dateData = Array.from(
+  const date = Array.from(
     {length: nrows},
     (_, i) => Date.now() - 1000 * 60 * 60 * 24 * i
   );
 
-  return tableToIPC(tableFromArrays({
-    precipitation: precipitationData,
-    date: dateData
-  }));
+  return tableToIPC(
+    tableFromArrays({ precipitation, date })
+  );
 }
 
-function generateDuplicateArrowData(nrows) {
-  const fields = [
-    new Field('precipitation', new Float32(), true),
-    new Field('precipitation', new Float32(), true)
-  ];
-
-  const precipitationData0 = Array.from(
+function generateDuplicateIPCData(nrows) {
+  const precipitation = () => Array.from(
     {length: nrows},
     () => Number((Math.random() * 20).toFixed(1))
   );
 
-  const precipitationData1 = Array.from(
-    {length: nrows},
-    () => Number((Math.random() * 20).toFixed(1))
-  );
-
-  let table = new Table(new Schema(fields));
-
-  table = table.setChildAt(0, vectorFromArray(precipitationData0, new Float32()));
-  table = table.setChildAt(1, vectorFromArray(precipitationData1, new Float32()));
+  const table = tableFromColumns([
+    ['precipitation', columnFromArray(precipitation(), float32())],
+    ['precipitation', columnFromArray(precipitation(), float32())]
+  ]);
 
   return tableToIPC(table);
 }
