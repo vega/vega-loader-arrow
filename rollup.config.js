5234844a7676fc44ea5ff1835dc3c09e30a65e84
Dominik Moritz <domoritz@gmail.com>
2025-06-22 14:58:17 +0200

    chore: no need to compile a raw js file

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
-import pkg from './package.json' with {type: 'json'};
-
 export default [{
-  input: 'src/index.js',
-  plugins: [nodeResolve({ modulesOnly: true })],
-  external: Object.keys(pkg.dependencies),
-  output: {
-    file: pkg.exports.default,
-    format: 'esm',
-    sourcemap: true,
-  }
-}, {
   input: 'src/index.js',
   plugins: [nodeResolve({ modulesOnly: true }), terser({ ecma: 2018 })],
   output: {
